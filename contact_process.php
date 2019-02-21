853902b40010b6c58e00d49c0e58ea58f1cd6c34
rodriguezsebastian <rodriguezsebastian@conicet.gov.ar>
2019-02-21 13:17:49 -0300

    Fist release finish

--- contact_process.php
+++ contact_process.php
@@ -1,10 +1,9 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "sebis_289@hotmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
     $headers = "From: $from";
@@ -13,7 +12,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    $subject = "You have a message from your Page";
 
     $logo = 'img/logo.png';
     $link = '#';
---
+++ js/motionBackTop.js
@@ -0,0 +1,152 @@
+ $(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-home");
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-home');
+            } else {
+                $('#back-home');
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-home a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+    });
+
+
+ $(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-sobremi");
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-sobremi');
+            } else {
+                $('#back-sobremi');
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-sobremi a').click(function () {
+            $('body,html').animate({
+                scrollTop:$('#sobremi').offset().top }, 800);
+            return false;
+        });
+    });
+
+    });
+
+
+  $(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-trayectoria");
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-trayectoria');
+            } else {
+                $('#back-trayectoria');
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-trayectoria a').click(function () {
+            $('body,html').animate({
+                scrollTop:$('#trayectoria').offset().top }, 800);
+            return false;
+        });
+    });
+
+    });
+
+  $(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-fotos");
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-fotos');
+            } else {
+                $('#back-fotos');
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-fotos a').click(function () {
+            $('body,html').animate({
+                scrollTop:$('#fotos').offset().top }, 800);
+            return false;
+        });
+    });
+
+    });
+
+  $(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-entrevistas");
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-entrevistas');
+            } else {
+                $('#back-entrevistas');
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-entrevistas a').click(function () {
+            $('body,html').animate({
+                scrollTop:$('#entrevistas').offset().top }, 800);
+            return false;
+        });
+    });
+
+    });
+
+    $(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-videos");
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-videos');
+            } else {
+                $('#back-videos');
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-videos a').click(function () {
+            $('body,html').animate({
+                scrollTop:$('#videos').offset().top }, 800);
+            return false;
+        });
+    });
+
+    });
