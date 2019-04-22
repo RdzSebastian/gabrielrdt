c9c1fe7e10f9753ab19fe07e953a7a1c19f78563
rodriguezsebastian <rodriguezsebastian@conicet.gov.ar>
2019-04-22 11:42:17 -0300

    Primeros pasos de traduccion con id

--- js/translate.js
+++ js/translate.js
@@ -1,33 +1,54 @@
     var dataReload = document.querySelectorAll("[data-reload]");
 
     var lenguaje = {
-      es: {
-        welcome: "home"
-      },
       en: {
-        welcome: "enhome"
+        home1: "CURRENT GENERAL MANAGER OF RIVER PLATES’S YOUTH DIVISION ",
+        home2: "Coach with over 38 years of experience in youth and profesional soccer",
+        sobreMi: "ABOUT ME",
+        sobreMi2: "Youth soccer coordinator. Trainer and recruiter of players. Manager of AF Parque Chas.",
+        sobreMi3: "My main goal is to transmit values, ethical principles and integrity in order to form good human being for the future"
       },
       de: {
-        welcome: "dehome"
+        home1: "herzlich willkommen",
+        home2: "Coach with over 38 years of experience in youth and profesional soccer",
+        sobreMi: "ABOUT ME",
+        sobreMi2: "Youth soccer coordinator. Trainer and recruiter of players. Manager of AF Parque Chas.",
+        sobreMi3: "My main goal is to transmit values, ethical principles and integrity in order to form good human being for the future "
       },
       fr: {
-        welcome: "frhome"
+        home1: "Bienvenue tout le monde",
+        home2: "Coach with over 38 years of experience in youth and profesional soccer",
+        sobreMi: "ABOUT ME",
+        sobreMi2: "Youth soccer coordinator. Trainer and recruiter of players. Manager of AF Parque Chas.",
+        sobreMi3: "My main goal is to transmit values, ethical principles and integrity in order to form good human being for the future"
       }
     }
 
     if(window.location.hash){
       if(window.location.hash === "#en"){
-        textContent = lenguaje.es.welcome;
+        home1.textContent = lenguaje.en.home1;
+        home2.textContent = lenguaje.en.home2;
+        sobreMi.textContent = lenguaje.en.sobreMi;
+        sobreMi2.textContent = lenguaje.en.sobreMi2;
+        sobreMi3.textContent = lenguaje.en.sobreMi3;
       }
     }
     if(window.location.hash){
       if(window.location.hash === "#de"){
-        home.textContent = lenguaje.es.welcome;
+        home1.textContent = lenguaje.de.home1;
+        home2.textContent = lenguaje.de.home2;
+        sobreMi.textContent = lenguaje.de.sobreMi;
+        sobreMi2.textContent = lenguaje.de.sobreMi2;
+        sobreMi3.textContent = lenguaje.de.sobreMi3;
       }
     }
     if(window.location.hash){
       if(window.location.hash === "#fr"){
-        home.textContent = lenguaje.es.welcome;
+        home1.textContent = lenguaje.fr.home1;
+        home2.textContent = lenguaje.fr.home2;
+        sobreMi.textContent = lenguaje.fr.sobreMi;
+        sobreMi2.textContent = lenguaje.fr.sobreMi2;
+        sobreMi3.textContent = lenguaje.fr.sobreMi3;
       }
     }
 
